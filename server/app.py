89e6818a035ff475bc903eb49d18ea3bee35cff2
Shubham_Buchwani <shbuchwani@gmail.com>
2026-04-07 12:25:23 +0530

    Fix: Rename entrypoint to main() in server/app.py for strict validation regex rules

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import app
 
-def start():
+def main():
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860, reload=False)
 
-if __name__ == "__main__":
-    start()
+if __name__ == '__main__':
+    main()
